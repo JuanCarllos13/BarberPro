a2d0ee4da0f656d86e4c90d3381ca4d8c689a1c8
Juan Carlos <juan2017amaral@gmail.com>
2023-01-03 16:34:33 -0400

    config node  init

---
+++ Server/src/server.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response, NextFunction } from "express";
+
+import { router } from "./routes";
+
+const app = express();
+
+app.use(router);
+
+app.listen(3333, () => console.log('SERVER ONLINE'))
